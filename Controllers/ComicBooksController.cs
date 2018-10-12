4a4999060cf148e814eb30a11683a3e491748557
Ed Cole (OzoneTank) <ed.cole@smashingboxes.com>
2018-10-11 20:59:35 -0400

    Added comic book detail view

--- Controllers/ComicBooksController.cs
+++ Controllers/ComicBooksController.cs
@@ -8,18 +8,10 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Index()
-        {
-            return View ();
-        }
 
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic books controllers!");
+            return View();
         }
     }
 }
