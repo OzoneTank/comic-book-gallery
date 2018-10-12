e4ba6e3ceab093e637e802345ba63d2a76eb37e2
Ed Cole (OzoneTank) <ed.cole@smashingboxes.com>
2018-10-11 20:46:30 -0400

    adding redirect

---
+++ Controllers/ComicBooksController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello from the comic books controllers!");
+        }
+    }
+}
